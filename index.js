2101e7a0aa2f46baf255578a15f75d101e7f29d2
yogevbd <yogevbd@wix.com>
2018-05-04 12:40:39 +0300

    ScreenVisibilityListener support

--- index.js
+++ index.js
@@ -2,7 +2,18 @@ import {Navigation} from 'react-native-navigation';
 import * as layoutGenerator from './layoutGenerator';
 import * as optionsGenerator from './optionsGenerator';
 
-const ScreenVisibilityListener = {};
+function ScreenVisibilityListener ({willAppear, didAppear, willDisappear, didDisappear}) {
+  this.register = function () {
+    Navigation.events().componentDidAppear((componentId, componentName) => {
+      willAppear({screen: componentName});
+      didAppear({screen: componentName});
+    });
+    Navigation.events().componentDidDisappear((componentId, componentName) => {
+      willDisappear({screen: componentName});
+      didDisappear({screen: componentName});
+    });
+  }
+};
 
 Navigation.startTabBasedApp = ({tabs, tabsStyle, appStyle, drawer}) => {
   Navigation.events().onAppLaunched(() => {
