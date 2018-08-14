b8d61e2d0ec9abf60df868f60b13464464dbf467
yogevbd <yogevbd@wix.com>
2018-08-14 16:27:12 +0300

    fixes ScreenVisibilityListener unregister

--- ScreenVisibilityListener.js
+++ ScreenVisibilityListener.js
@@ -1,19 +1,21 @@
 import {Navigation} from 'react-native-navigation';
 
+const listeners = [];
+
 export default function ScreenVisibilityListener({willAppear = () => {}, didAppear = () => {}, willDisappear = () => {}, didDisappear = () => {}}) {
   this.register = function () {
-    Navigation.events().registerComponentDidAppearListener(({componentId, componentName}) => {
+    listeners.push(Navigation.events().registerComponentDidAppearListener(({componentId, componentName}) => {
       willAppear({screen: componentName});
       didAppear({screen: componentName});
-    });
-    Navigation.events().registerComponentDidDisappearListener(({componentId, componentName}) => {
+    }));
+    listeners.push(Navigation.events().registerComponentDidDisappearListener(({componentId, componentName}) => {
       willDisappear({screen: componentName});
       didDisappear({screen: componentName});
-    });
+    }));
   };
 
   this.unregister = function () {
-    Navigation.events().registerComponentDidAppearListener((componentId, componentName) => {});
-    Navigation.events().registerComponentDidDisappearListener((componentId, componentName) => {});
+    listeners.forEach(listener => listener.remove && listener.remove());
+    listeners = [];
   };
 }
